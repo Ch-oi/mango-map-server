db53419b11480b124be9219fe85290218ded0d12
Ch-oi <gmu1233@gmail.com>
2020-07-19 19:19:09 +0800

    useless

--- back_end/router/BlogRouter.js
+++ back_end/router/BlogRouter.js
@@ -1,76 +1,115 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
 class BlogRouter {
-    constructor(blogService) {
-        this.blogService = blogService
-        this.router = router
-    }
-    route() {
-        this.router.get('/all', this.listBlogs.bind(this))
-        this.router.get('/:id', this.getBlog.bind(this))
-        this.router.post('/', this.addBlog.bind(this))
-        this.router.post('/images/:id', this.addBlogImages.bind(this))
-        this.router.post('/categories/:id', this.addBlogCategories.bind(this))
-        this.router.post('/comment/:id', this.addBlogComment.bind(this))
+  constructor(blogService) {
+    this.blogService = blogService;
+    this.router = router;
+  }
+  route() {
+    this.router.get('/all', this.listBlogs.bind(this));
+    this.router.get('/:id', this.getBlog.bind(this));
+    this.router.post('/', this.addBlog.bind(this));
+    this.router.post('/images/:id', this.addBlogImages.bind(this));
+    this.router.post('/categories/:id', this.addBlogCategories.bind(this));
+    this.router.post('/comment/:id', this.addBlogComment.bind(this));
 
-        this.router.get('/categories', this.listCategories.bind(this))
-        this.router.post('/categories', this.addCategories.bind(this))
-    }
+    this.router.get('/categories', this.listCategories.bind(this));
+    this.router.post('/categories', this.addCategories.bind(this));
+  }
 
-    listBlogs(req, res) {
-        return this.blogService.listBlogs()
-            .then(blogs => {res.send(blogs)})
-            .catch(err => {console.log(err)})
-    }
-    getBlog(req, res) {
-        let blog_id = req.params.id
-        return this.blogService.getBlog(blog_id)
-            .then(blog => {res.send(blog)})
-            .catch(err => {console.log(err)})
-    }
-    addBlog(req, res) {
-        let new_blog = { ...req.body }
-        return this.blogService.addBlog(new_blog)
-            .then(newBlog => {res.send(newBlog)})
-            .catch(err => {console.log(err)})
-    }
-    addBlogImages(req, res) {
-        let urls = [...req.body.urls]
-        let blog_id = req.params.id
-        return this.blogService.addBlogImages(urls, blog_id)
-            .then(newImgs => {res.send(newImgs)})
-            .catch(err => {console.log(err)})
-    }
+  listBlogs(req, res) {
+    return this.blogService
+      .listBlogs()
+      .then((blogs) => {
+        res.send(blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  getBlog(req, res) {
+    let blog_id = req.params.id;
+    return this.blogService
+      .getBlog(blog_id)
+      .then((blog) => {
+        res.send(blog);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  addBlog(req, res) {
+    let new_blog = { ...req.body };
+    return this.blogService
+      .addBlog(new_blog)
+      .then((newBlog) => {
+        res.send(newBlog);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  addBlogImages(req, res) {
+    let urls = [...req.body.urls];
+    let blog_id = req.params.id;
+    return this.blogService
+      .addBlogImages(urls, blog_id)
+      .then((newImgs) => {
+        res.send(newImgs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    addBlogComment(req, res) {
-        let comment = { ...req.body }
-        return this.blogService.addBlogComment(comment)
-            .then(newComment => {res.send(newComment)})
-            .catch(err => {console.log(err)})
-    }
+  addBlogComment(req, res) {
+    let comment = { ...req.body };
+    return this.blogService
+      .addBlogComment(comment)
+      .then((newComment) => {
+        res.send(newComment);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    addBlogCategories(req, res) {
-        let cates_id = [...req.body.categories_id]
-        let blog_id = req.params.id
-        return this.blogService.addBlogCategories(cates_id, blog_id)
-            .then(newBlogCates => {res.send(newBlogCates)})
-            .catch(err =>{ console.log(err)})
-    }
+  addBlogCategories(req, res) {
+    let cates_id = [...req.body.categories_id];
+    let blog_id = req.params.id;
+    return this.blogService
+      .addBlogCategories(cates_id, blog_id)
+      .then((newBlogCates) => {
+        res.send(newBlogCates);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
+  listCategories(req, res) {
+    return this.blogService
+      .listCategories()
+      .then((categories) => {
+        res.send(categories);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    listCategories(req, res) {
-        return this.blogService.listCategories()
-            .then(categories => {res.send(categories)})
-            .catch(err => {console.log(err)})
-    }
+  addCategories(req, res) {
+    let new_categories = [...req.body.categories];
 
-    addCategories(req, res) {
-        let new_categories = [...req.body.categories]
-
-        return this.blogService.addCategories(new_categories)
-            .then(new_categories => {res.send(new_categories)})
-            .catch(err => {console.log(err)})
-    }
+    return this.blogService
+      .addCategories(new_categories)
+      .then((new_categories) => {
+        res.send(new_categories);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
-module.exports = BlogRouter
+module.exports = BlogRouter;
--- back_end/server.js
+++ back_end/server.js
@@ -1,37 +1,35 @@
-const express = require('express')
-const port = process.env.PORT || 8000
-const app = express()
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-const knex = require('./database/config')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const knex = require('./database/config');
 
-const UserRouter = require('./router/UserRouter')
-const BlogRouter = require('./router/BlogRouter')
-const ChatroomRouter = require('./router/ChatroomRouter')
-const MapRouter = require('./router/MapRouter')
+const UserRouter = require('./router/UserRouter');
+const BlogRouter = require('./router/BlogRouter');
+const ChatroomRouter = require('./router/ChatroomRouter');
+const MapRouter = require('./router/MapRouter');
 
-const UserService = require('./services/UserService')
-const BlogService = require('./services/BlogService')
-const ChatroomService = require('./services/ChatroomService')
-const MapService = require('./services/MapService')
+const UserService = require('./services/UserService');
+const BlogService = require('./services/BlogService');
+const ChatroomService = require('./services/ChatroomService');
+const MapService = require('./services/MapService');
 
-const userService = new UserService()
-const blogService = new BlogService()
-const chatroomService = new ChatroomService()
-const mapService = new MapService()
+const userService = new UserService();
+const blogService = new BlogService();
+const chatroomService = new ChatroomService();
+const mapService = new MapService();
 
 // app.use('/user', new UserRouter(userService ).route());
-// app.use('/chatroom', new ChatroomRouter(chatroomService ).route());
-// app.use('/blog', new BlogRouter(blogService ).route());
+app.use('/chatroom', new ChatroomRouter(chatroomService).route());
+// app.use('/blog', new BlogRouter(blogService).route());
 // app.use('/map', new MapRouter(mapService ).route());
 
-
 app.get('/', async (req, res) => {
-    res.send()
-})
+  res.send();
+});
 
 app.listen(port, function () {
-    console.log('listening on port' + port)
-})
-
+  console.log('listening on port' + port);
+});
